4a22062998025d0854af9392cabd5663d57b9c4a
zhaoqin192 <zhaoqin192@gmail.com>
2016-06-13 12:37:07 +0800

    init project

---
+++ Xpai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Xpai
+//
+//  Created by zhaoqin on 6/13/16.
+//  Copyright © 2016 Neotel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
